62ddcdf2a9e672a877bf0ea19435f74d15188701
icrausw <cordelia35794@gmail.com>
2022-03-15 15:42:43 +0900

    update

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,5 +1,110 @@
+import { useEffect, useState } from "react";
+import { Helmet } from "react-helmet";
+import { useQuery } from "react-query";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { fetchCoins } from "../api";
+
+const Container = styled.div`
+  padding: 0px 20px;
+`;
+
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ConisList = styled.ul``;
+
+const Coin = styled.li`
+  background-color: white;
+  color: ${(props) => props.theme.bgColor};
+  border-radius: 15px;
+  margin-bottom: 10px;
+  a {
+    display: flex;
+    align-items: center;
+    padding: 20px;
+    transition: color 0.2s ease-in;
+  }
+  &:hover {
+    a {
+      color: ${(props) => props.theme.accentColor};
+    }
+  }
+`;
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
+
+const Img = styled.img`
+  width: 35px;
+  height: 35px;
+  margin-right: 10px;
+`;
+
+interface ICoin {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+}
+
 function Coins() {
-  return <h1>Coins</h1>;
+  /*  const [coins, setCoins] = useState<ICoin[]>([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    (async () => {
+
+      setCoins(json.slice(0, 100));
+      setLoading(false);
+    })();
+  }, []); */
+  const { isLoading, data } = useQuery<ICoin[]>("allCoins", fetchCoins);
+
+  return (
+    <Container>
+      <Helmet>
+        <title>코인</title>
+      </Helmet>
+      <Header>
+        <Title>코인</Title>
+      </Header>
+      {isLoading ? (
+        <Loader>"Loading...."</Loader>
+      ) : (
+        <ConisList>
+          {data?.slice(0, 100).map((coin) => (
+            <Coin key={coin.id}>
+              <Link
+                to={{
+                  pathname: `/${coin.id}`,
+                  state: { name: coin.name },
+                }}
+              >
+                <Img
+                  src={`https://cryptoicon-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`}
+                />
+                {coin.name} &rarr;
+              </Link>
+            </Coin>
+          ))}
+        </ConisList>
+      )}
+    </Container>
+  );
 }
 
 export default Coins;

5f1c3aaadd1e37a614b4b95a5fc91f615a595fe8
icrausw <cordelia35794@gmail.com>
2022-03-18 23:18:22 +0900

    ToDoList

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { createGlobalStyle, ThemeProvider } from "styled-components";
 import { darkTheme } from "./theme";
 import { useState } from "react";
 import { useRecoilValue } from "recoil";
-import ToDoList from "./ToDoList";
+import ToDoList from "./components/ToDoList";
 
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');

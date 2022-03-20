d40a928107b526678c69e2bc0e37b1367d1aca5e
icrausw <cordelia357@naver.com>
2022-03-21 01:15:58 +0900

    set NOMFLIX CLONE

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -1,9 +1,16 @@
-// import original module declarations
 import "styled-components";
+
 declare module "styled-components" {
   export interface DefaultTheme {
-    bgColor: string;
-    boardColor: string;
-    cardColor: string;
+    red: string;
+    black: {
+      veryDark: string;
+      darker: string;
+      lighter: string;
+    };
+    white: {
+      darker: string;
+      lighter: string;
+    };
   }
 }
